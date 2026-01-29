b438a803678f290d14cc2bcdc8f1e4d599966ef2
iqweenex <iqweenex@gmail.com>
2026-01-29 18:05:10 +0500

    fix_3: тест-кейс сделан в одном методе как единое целое, исправлены недочеты, учтены комментарии

--- conftest.py
+++ conftest.py
@@ -4,14 +4,18 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 
+TIMEOUT = 10
+WINDOW_WIDTH = 1920
+WINDOW_HEIGHT = 1080
 
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.maximize_window()
+    driver.set_window_size(WINDOW_WIDTH, WINDOW_HEIGHT)
     yield driver
     driver.quit()
 
+
 @pytest.fixture()
 def wait(driver):
-    return WebDriverWait(driver, 10)
+    return WebDriverWait(driver, TIMEOUT)
